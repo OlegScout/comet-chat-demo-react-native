cdd1d1981769c9a942d71f01fad7d18a29d55c64
Oleg Kruchay <{ID}+{username}@users.noreply.github.com>
2022-02-17 16:15:30 +0200

    chore(Chats/GSCometChatConversation{List, ListItem, ListWithMessages}.js): getting rid the unnnecessary components, moving customized data into comet-chat-ui-kit directory
    v3.5.1: Updates the 'Projects' screen UI

--- CometChatWorkspace/cometchat-app/components/Chats/index.js
+++
@@ -1,3 +0,0 @@
-export { default as GSCometChatConversationList } from './GSCometChatConversationList';
-export { default as GSCometChatConversationListItem } from './GSCometChatConversationListItem';
-export { default as GSCometChatConversationListWithMessages } from './GSCometChatConversationListWithMessages';
